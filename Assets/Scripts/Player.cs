e6e49d1eb50cecaaf58956063d65fbde4b249ee9
Les <stitches.phone@googlemail.com>
2026-03-25 19:46:40 +0000

    Added player bindings

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,32 +5,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Dateils")]
     [SerializeField]
     private Vector3 _startPosition = new Vector3(0, -3, 0);
     [SerializeField]
     private float _playerSpeed = 2.5f;
     [SerializeField]
-    Vector3 _direction = new Vector3();
+    Vector3 _playerDirection = new Vector3();
+    [SerializeField]
+    private Vector3 _bindPosition = new Vector3();
 
-    // Start is called before the first frame update
+    [Header("Player Binds")]
+    [SerializeField]
+    private float _leftPlayerBind;
+    [SerializeField]
+    private float _rightPlayerBind;
+    [SerializeField]
+    private float _upperPlayerBind;
+    [SerializeField]
+    private float _lowerPlayerBind;
+
     void Start()
     {
         // assign starting position
         transform.position = _startPosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float hMove;
-        float vMove;
+        // get inputs
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
 
-        hMove = Input.GetAxis("Horizontal");
-        vMove = Input.GetAxis("Vertical");
+        // apply inputs to Vector3
+        _playerDirection.x = hMove;
+        _playerDirection.y = vMove;
 
-        _direction.x = hMove;
-        _direction.y = vMove;
+        // move player
+        transform.Translate(_playerDirection * _playerSpeed * Time.deltaTime);
 
-        transform.Translate(_direction * _playerSpeed * Time.deltaTime);
+        // check player binds and restrain
+        _bindPosition = transform.position;
+
+        if (transform.position.x <= _leftPlayerBind)
+        {
+            _bindPosition.x = _leftPlayerBind;
+        }
+        else if (transform.position.x >= _rightPlayerBind)
+        {
+            _bindPosition.x = _rightPlayerBind;
+        }
+
+        if (transform.position.y >= _upperPlayerBind)
+        {
+            _bindPosition.y = _upperPlayerBind;
+        }
+        else if (transform.position.y <= _lowerPlayerBind)
+        {
+            _bindPosition.y = _lowerPlayerBind;
+        }
+
+        // apply binding restrictions
+        transform.position = _bindPosition;
     }
 }
