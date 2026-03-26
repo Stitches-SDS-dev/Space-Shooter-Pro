bea700a4fba23d663276ac37e01ca9e001a8de88
Les <stitches.phone@googlemail.com>
2026-03-26 20:50:41 +0000

    Added player firing and Laser behaviour

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _laserSpeed;
+    [SerializeField]
+    private float _despawnPosition;
+    private Vector3 _direction = Vector3.up;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move laser
+        transform.Translate(_direction * _laserSpeed * Time.deltaTime);
+
+        // destroy after leaving screen
+        if (transform.position.y > _despawnPosition)
+        {
+            Destroy(this.gameObject, 1f);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,6 +25,18 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _lowerPlayerBind;
 
+    [Header("Additional Settings")]
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _yLaserOffset;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+
+
+    private Vector3 _laserOffset = new Vector3();
+    private float _canFire = 0f;
+
     void Start()
     {
         // assign starting position
@@ -32,6 +44,25 @@ public class Player : MonoBehaviour
     }
 
     void Update()
+    {
+        PlayerMovement();
+
+        // check for input and rate of fire delay
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    private void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        _laserOffset = transform.position;
+        _laserOffset.y += _yLaserOffset;
+        Instantiate(_laserPrefab, _laserOffset, Quaternion.identity);
+    }
+
+    private void PlayerMovement()
     {
         // get inputs
         float hMove = Input.GetAxis("Horizontal");
@@ -44,7 +75,7 @@ public class Player : MonoBehaviour
         // move player
         transform.Translate(_playerDirection * _playerSpeed * Time.deltaTime);
 
-        // check player binds and restrain
+        // check player binds and set restraints
         _bindPosition = transform.position;
 
         if (transform.position.x <= _leftPlayerBind)
